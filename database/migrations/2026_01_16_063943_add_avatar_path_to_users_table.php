ccf16ae90ddac182edda92114c36b709a9d340a8
Khairan Aja <khairking6@gmail.com>
2026-01-16 13:40:14 +0700

    fix(user): add missing avatar_path column to migrations

---
+++ database/migrations/2026_01_16_063943_add_avatar_path_to_users_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            // Kita kasih nullable() biar user lama yang nggak punya foto nggak error
+            $table->string('avatar_path')->after('email')->nullable();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('avatar_path');
+        });
+    }
+};
