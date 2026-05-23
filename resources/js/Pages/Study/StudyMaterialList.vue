6dbdbf027223f57f176c98f0a7d6dd0f1e956d8b
Khairan Aja <khairking6@gmail.com>
2026-05-23 18:23:44 +0800

    fix: unclosed button tag

--- resources/js/Pages/Study/StudyMaterialList.vue
+++ resources/js/Pages/Study/StudyMaterialList.vue
@@ -183,6 +183,7 @@ const copyCardLink = (id) => {
                         title="Delete Material"
                     >
                         <Trash2 class="h-4 w-4" />
+                    </button>
                 </div>
             </div>
         </div>
