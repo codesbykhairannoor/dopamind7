bad2192bb4b759f7adf5b9a7a974d25cb816ec17
Khairan Aja <khairking6@gmail.com>
2026-08-01 14:33:02 +0700

    fix(finance): prevent datepicker cutoff in modal and add missing translations

--- resources/js/Pages/Finance/TransactionModal.vue
+++ resources/js/Pages/Finance/TransactionModal.vue
@@ -140,9 +140,9 @@ const onInput = (e) => {
                             <span class="truncate">{{ dateDisplay }}</span>
                             <span class="text-slate-400 dark:text-slate-600">📅</span>
                         </button>
-                        <transition enter-active-class="transition ease-out duration-200" enter-from-class="opacity-0 translate-y-4 sm:translate-y-2" enter-to-class="opacity-100 translate-y-0" leave-active-class="transition ease-in duration-150" leave-from-class="opacity-100 translate-y-0" leave-to-class="opacity-0 translate-y-4 sm:translate-y-2">
-                            <div v-if="showDatePicker" class="fixed inset-0 z-[100] flex items-center justify-center p-4 sm:p-0 sm:absolute sm:bottom-full sm:right-0 sm:mb-2 sm:origin-bottom-right sm:block sm:inset-auto">
-                                <div class="fixed inset-0 bg-slate-900/40 dark:bg-black/60  sm:hidden" @click="showDatePicker = false"></div>
+                        <transition enter-active-class="transition ease-out duration-200" enter-from-class="opacity-0 translate-y-4" enter-to-class="opacity-100 translate-y-0" leave-active-class="transition ease-in duration-150" leave-from-class="opacity-100 translate-y-0" leave-to-class="opacity-0 translate-y-4">
+                            <div v-if="showDatePicker" class="fixed inset-0 z-[110] flex items-center justify-center p-4">
+                                <div class="fixed inset-0 bg-slate-900/40 dark:bg-black/60 backdrop-blur-sm" @click="showDatePicker = false"></div>
                                 <FinanceDatePicker 
                                     :show="true" 
                                     :modelValue="form.date"
